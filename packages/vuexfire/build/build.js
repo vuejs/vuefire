c2f32f2abf521c478479d03b58aea1e00b70606c
Eduardo San Martin Morote <posva13@gmail.com>
2017-01-02 12:09:50 +0100

    🔧 Add rollup for building

---
+++ packages/vuexfire/build/build.js
@@ -0,0 +1,45 @@
+const rollup = require('rollup').rollup
+const buble = require('rollup-plugin-buble')
+const uglify = require('uglify-js')
+const packageData = require('../package.json')
+const { version, author, name } = packageData
+// remove the email at the end
+const authorName = author.replace(/\s+<.*/, '')
+
+const {
+  logError,
+  write
+} = require('./utils')
+
+const banner =
+      '/*!\n' +
+      ` * ${name} v${version}\n` +
+      ` * (c) ${new Date().getFullYear()} ${authorName}\n` +
+      ' * Released under the MIT License.\n' +
+      ' */'
+
+rollup({
+  entry: 'src/index.js',
+  plugins: [
+    buble()
+  ]
+}).then(function (bundle) {
+  var code = bundle.generate({
+    format: 'umd',
+    exports: 'named',
+    banner: banner,
+    moduleName: name
+  }).code
+  return write(`dist/${name}.js`, code).then(function () {
+    return code
+  })
+}).then(function (code) {
+  var minified = uglify.minify(code, {
+    fromString: true,
+    output: {
+      preamble: banner,
+      ascii_only: true
+    }
+  }).code
+  return write(`dist/${name}.min.js`, minified)
+}).catch(logError)
---
+++ packages/vuexfire/build/utils/index.js
@@ -0,0 +1,7 @@
+const write = require('./write.js')
+const { logError } = require('./log.js')
+
+module.exports = {
+  write,
+  logError
+}
