a94d6f6cc5bcd31b03dca84eab8625a04951d940
Eduardo San Martin Morote <posva13@gmail.com>
2018-12-27 17:56:33 +0100

    feat(vuefire-core): rtdb support for arrays

--- packages/@posva/vuefire-core/src/rtdb.js
+++ packages/@posva/vuefire-core/src/rtdb.js
@@ -19,7 +19,7 @@ export function rtdbBindAsArray ({ vm, key, collection, resolve, reject, ops }) 
   const array = []
   ops.set(vm, key, array)
 
-  collection.on(
+  const childAdded = collection.on(
     'child_added',
     (snapshot, prevKey) => {
       const index = prevKey ? indexForKey(array, prevKey) + 1 : 0
@@ -28,5 +28,43 @@ export function rtdbBindAsArray ({ vm, key, collection, resolve, reject, ops }) 
     reject
   )
 
+  const childRemoved = collection.on(
+    'child_removed',
+    snapshot => {
+      ops.remove(array, indexForKey(array, snapshot.key))
+    },
+    reject
+  )
+
+  const childChanged = collection.on(
+    'child_changed',
+    snapshot => {
+      ops.set(
+        array,
+        indexForKey(array, snapshot.key),
+        createRecordFromRTDBSnapshot(snapshot)
+      )
+    },
+    reject
+  )
+
+  const childMoved = collection.on(
+    'child_moved',
+    (snapshot, prevKey) => {
+      const index = indexForKey(array, snapshot.key)
+      const oldRecord = ops.remove(array, index)[0]
+      const newIndex = prevKey ? indexForKey(array, prevKey) + 1 : 0
+      ops.add(array, newIndex, oldRecord)
+    },
+    reject
+  )
+
   collection.once('value', resolve)
+
+  return () => {
+    collection.off('child_added', childAdded)
+    collection.off('child_changed', childChanged)
+    collection.off('child_removed', childRemoved)
+    collection.off('child_moved', childMoved)
+  }
 }
--- packages/@posva/vuefire-core/test/rtdb/as-array.spec.js
+++ packages/@posva/vuefire-core/test/rtdb/as-array.spec.js
@@ -10,10 +10,10 @@ function createSnapshotFromPrimitive (value, key) {
 }
 
 describe('RTDB collection', () => {
-  let collection, vm, resolve, reject, ops, unbind
+  let collection, vm, resolve, reject, unbind
+  const ops = createOps(walkSet)
   beforeEach(async () => {
     collection = new MockFirebase().child('data')
-    ops = createOps(walkSet)
     vm = {}
     await new Promise((res, rej) => {
       resolve = jest.fn(res)
@@ -40,5 +40,75 @@ describe('RTDB collection', () => {
       { name: 'two' },
       { name: 'three' }
     ])
+  })
+
+  it('removes elements', () => {
+    collection.push({ name: 'one' })
+    collection.push({ name: 'two' })
+    collection.push({ name: 'three' })
+    collection.flush()
+    collection.child(vm.items[1]['.key']).remove()
+    collection.flush()
+    expect(vm.items).toEqual([{ name: 'one' }, { name: 'three' }])
+  })
+
+  it('stops listening to events when unbound', async () => {
+    collection.push({ name: 'one' })
+    collection.push({ name: 'two' })
+    collection.push({ name: 'three' })
+    collection.flush()
+    const items = new MockFirebase().child('other')
+    items.push({ other: 'one' })
+    items.push({ other: 'two' })
+    items.push({ other: 'three' })
+    items.flush()
+
+    unbind()
+    await new Promise((resolve, reject) => {
+      rtdbBindAsArray({
+        vm,
+        key: 'items',
+        collection: items,
+        resolve,
+        reject,
+        ops
+      })
+      items.flush()
+    })
+
+    expect(vm.items).toEqual([
+      { other: 'one' },
+      { other: 'two' },
+      { other: 'three' }
+    ])
+  })
+
+  it.skip('reorder elements', async () => {
+    collection.push({ value: 3 })
+    collection.push({ value: 1 })
+    collection.push({ value: 2 })
+    await new Promise((res, rej) => {
+      resolve = jest.fn(res)
+      reject = jest.fn(rej)
+      rtdbBindAsArray({
+        vm,
+        key: 'items',
+        collection: collection.orderByChild('value'),
+        resolve,
+        reject,
+        ops
+      })
+      collection.flush()
+    })
+
+    expect(vm.items).toEqual([{ value: 1 }, { value: 2 }, { value: 3 }])
+  })
+
+  it('updates an item', () => {
+    collection.push({ name: 'foo' })
+    collection.flush()
+    collection.child(vm.items[0]['.key']).set({ name: 'bar' })
+    collection.flush()
+    expect(vm.items).toEqual([{ name: 'bar' }])
   })
 })
