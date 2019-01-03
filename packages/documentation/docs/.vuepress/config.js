97be23ae1c1b549e5c004805b2d68431ebc6bcb1
Eduardo San Martin Morote <posva13@gmail.com>
2019-01-03 11:37:01 +0100

    docs: add config

---
+++ packages/documentation/docs/.vuepress/config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+
+module.exports = {
+  head: [
+    ['link', { rel: 'icon', href: `/logo.png` }],
+    // ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    [
+      'meta',
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }
+    ],
+    // ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+    // ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    // ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
+  themeConfig: {
+    repo: 'vuejs/vuefire',
+    editLinks: true,
+    docsDir: 'docs'
+    // #697 Provided by the official algolia team.
+    // algolia: {
+    //   apiKey: '3a539aab83105f01761a137c61004d85',
+    //   indexName: 'vuepress'
+    // },
+  },
+  plugins: {
+    '@vuepress/back-to-top': true
+    // '@vuepress/pwa': {
+    //   serviceWorker: true,
+    //   updatePopup: true
+    // },
+    // '@vuepress/notification': true,
+  }
+}
