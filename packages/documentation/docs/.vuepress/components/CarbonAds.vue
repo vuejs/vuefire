db356f12e1b0ce8361489c62d7b57484813d4a8a
Eduardo San Martin Morote <posva13@gmail.com>
2019-02-01 12:45:47 +0100

    docs: add carbon ads

---
+++ packages/documentation/docs/.vuepress/components/CarbonAds.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="carbon_ad" ref="carbonads">
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    if (this.$refs.carbonads.children.length) return
+    const script = document.createElement('script')
+    script.setAttribute('type', 'text/javascript')
+    script.setAttribute(
+      'src',
+      '//cdn.carbonads.com/carbon.js??serve=CK7DL23N&placement=vuefirevuejsorg'
+    )
+
+    script.setAttribute('id', '_carbonads_js')
+    this.$refs.carbonads.appendChild(script)
+  },
+}
+</script>
+
+<style lang="stylus">
+.carbon_ad
+  min-height 102px
+  padding 1.5rem 1.5rem 0
+  margin-bottom -0.5rem
+  font-size 0.75rem
+  @media (max-width: $MQMobile)
+    .carbon-img img
+        width 100px
+        height 77px
+  a
+    color #444
+    font-weight normal
+    display inline
+  .carbon-img
+    float left
+    margin-right 1rem
+    border 1px solid $borderColor
+    img
+      display block
+  .carbon-poweredby
+    color #999
+    display block
+    margin-top 0.5em
+
+  .carbon_ad
+    .carbon-img img
+      width 100px
+      height 77px
+</style>
