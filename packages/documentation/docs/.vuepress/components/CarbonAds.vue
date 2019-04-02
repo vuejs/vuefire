beeb900e8f4e9c3cf4879861c96bab23eca00437
Eduardo San Martin Morote <posva13@gmail.com>
2019-04-02 22:25:59 +0200

    docs: refresh ads on route change

--- packages/documentation/docs/.vuepress/components/CarbonAds.vue
+++ packages/documentation/docs/.vuepress/components/CarbonAds.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="carbon_ad" ref="carbonads">
-  </div>
+  <div class="carbon_ad" ref="carbonads"></div>
 </template>
 
 <script>
@@ -17,36 +16,57 @@ export default {
     script.setAttribute('id', '_carbonads_js')
     this.$refs.carbonads.appendChild(script)
   },
+  watch: {
+    $route(to, from) {
+      if (to.path !== from.path && typeof _carbonads === 'object') {
+        _carbonads.refresh()
+      }
+    },
+  },
 }
 </script>
 
 <style lang="stylus">
-.carbon_ad
-  min-height 102px
-  padding 1.5rem 1.5rem 0
-  margin-bottom -0.5rem
-  font-size 0.75rem
-  @media (max-width: $MQMobile)
-    .carbon-img img
-        width 100px
-        height 77px
-  a
-    color #444
-    font-weight normal
-    display inline
-  .carbon-img
-    float left
-    margin-right 1rem
-    border 1px solid $borderColor
-    img
-      display block
-  .carbon-poweredby
-    color #999
-    display block
-    margin-top 0.5em
+.carbon_ad {
+  min-height: 102px;
+  padding: 1.5rem 1.5rem 0;
+  margin-bottom: -0.5rem;
+  font-size: 0.75rem;
 
-  .carbon_ad
-    .carbon-img img
-      width 100px
-      height 77px
+  @media (max-width: $MQMobile) {
+    .carbon-img img {
+      width: 100px;
+      height: 77px;
+    }
+  }
+
+  a {
+    color: #444;
+    font-weight: normal;
+    display: inline;
+  }
+
+  .carbon-img {
+    float: left;
+    margin-right: 1rem;
+    border: 1px solid $borderColor;
+
+    img {
+      display: block;
+    }
+  }
+
+  .carbon-poweredby {
+    color: #999;
+    display: block;
+    margin-top: 0.5em;
+  }
+
+  .carbon_ad {
+    .carbon-img img {
+      width: 100px;
+      height: 77px;
+    }
+  }
+}
 </style>
