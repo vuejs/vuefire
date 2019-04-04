684dc4a768d116539c285d27dbfe324544fabaa8
Eduardo San Martin Morote <posva13@gmail.com>
2019-04-04 12:12:08 +0200

    docs: add icons

--- packages/documentation/docs/.vuepress/config.js
+++ packages/documentation/docs/.vuepress/config.js
@@ -13,9 +13,25 @@ module.exports = {
       'meta',
       { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
     ],
-    // ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
-    // ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-    // ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    [
+      'link',
+      { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` },
+    ],
+    [
+      'link',
+      {
+        rel: 'mask-icon',
+        href: '/icons/safari-pinned-tab.svg',
+        color: '#3eaf7c',
+      },
+    ],
+    [
+      'meta',
+      {
+        name: 'msapplication-TileImage',
+        content: '/icons/msapplication-icon-144x144.png',
+      },
+    ],
     ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
   ],
   themeConfig: {
