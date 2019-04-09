05b56c73bab6c94c568733897e588124035941d5
Eduardo San Martin Morote <posva13@gmail.com>
2019-04-09 18:09:50 +0200

    feat(vuefire): add types for plugin options

--- packages/vuefire/types/index.d.ts
+++ packages/vuefire/types/index.d.ts
@@ -2,5 +2,11 @@ import * as firebase from 'firebase'
 import { PluginFunction } from 'vue'
 import './vue'
 
-export declare const firestorePlugin: PluginFunction<never>
-export declare const rtdbPlugin: PluginFunction<never>
+
+interface Options {
+  bindName?: string
+  unbindName?: string
+}
+
+export declare const firestorePlugin: PluginFunction<Options>
+export declare const rtdbPlugin: PluginFunction<Options>
