b9f2c9885177988f5fb0168a525325a848a0d2da
Christoph Lipp <christoph@lipp.ws>
2019-04-17 11:59:53 +0200

    fix(vuexfire): include maxRefDepth option in typings (#260)
    * added missing options in types
    
    * use an interface name that makes sense

--- packages/vuexfire/types/index.d.ts
+++ packages/vuexfire/types/index.d.ts
@@ -3,14 +3,20 @@ import { firestore } from 'firebase'
 
 export declare const vuexfireMutations: MutationTree<any>
 
+export interface BindFirestoreRefOptions {
+  maxRefDepth: number
+}
+
 interface FirestoreActionContext<S, R> extends ActionContext<S, R> {
   bindFirestoreRef(
     key: string,
-    ref: firestore.Query
+    ref: firestore.Query,
+    options?: BindFirestoreRefOptions
   ): Promise<firestore.DocumentData[]>
   bindFirestoreRef(
     key: string,
-    ref: firestore.DocumentReference
+    ref: firestore.DocumentReference,
+    options?: BindFirestoreRefOptions
   ): Promise<firestore.DocumentData>
   unbindFirestoreRef(key: string): void
 }
