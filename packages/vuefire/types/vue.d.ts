cc00b06519b3a9d674316014c55a70b18386f443
trickstival <trick_stival@hotmail.com>
2019-07-11 17:43:07 -0300

    No need for comma

--- packages/vuefire/types/vue.d.ts
+++ packages/vuefire/types/vue.d.ts
@@ -7,12 +7,12 @@ import { firestore, database } from 'firebase'
 
 export interface BindFirestoreRefOptions {
   maxRefDepth?: number
-  reset?: boolean | (() => any),
+  reset?: boolean | (() => any)
   createSnapshot?: (documentSnapshot: firestore.DocumentSnapshot) => Record<string, any>
 }
 
 export interface BindRTDBRefOptions {
-  reset?: boolean | (() => any),
+  reset?: boolean | (() => any)
   createSnapshot?: (dataSnapshot: database.DataSnapshot) => Record<string, any>
 }
 
