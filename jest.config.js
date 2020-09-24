2862cd17228c81d39b0ea74443849706525e3cff
Eduardo San Martin Morote <posva13@gmail.com>
2020-09-24 22:50:44 +0200

    test: ignore index file

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
+/** @type {jest.Parameters} */
 module.exports = {
   preset: 'ts-jest',
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coveragePathIgnorePatterns: ['/node_modules/', 'src/index.ts'],
   testMatch: ['<rootDir>/__tests__/**/*.spec.ts'],
   globals: {
     'ts-jest': {
