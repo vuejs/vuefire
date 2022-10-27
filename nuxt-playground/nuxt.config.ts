93d54ad28f61beec10f10cf21991d79453d1fe2e
Eduardo San Martin Morote <posva13@gmail.com>
2022-10-27 10:59:50 +0200

    chore: fix nuxt imports in playground

--- nuxt-playground/nuxt.config.ts
+++ nuxt-playground/nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+import { fileURLToPath } from 'url'
+
 export default defineNuxtConfig({
   pageTransition: null,
   layoutTransition: null,
+  alias: {
+    vuefire: fileURLToPath(new URL('../src/index.ts', import.meta.url)),
+  },
 })
