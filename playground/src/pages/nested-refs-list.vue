f53144f94da7fb3005dff8dc813982835bb82793
Eduardo San Martin Morote <posva13@gmail.com>
2022-10-31 19:19:22 +0100

    chore: playground nested refs

---
+++ playground/src/pages/nested-refs-list.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { collection, doc, getDoc } from 'firebase/firestore'
+import { useCollection, useDocument } from 'vuefire'
+import { useFirestore } from '@/firebase'
+
+const db = useFirestore()
+const nestedRefs = collection(db, 'playground/nested-refs/list')
+
+const nestedList = useCollection(nestedRefs)
+</script>
+
+<template>
+  <p>List with nested refs</p>
+  <pre>{{ nestedList }}</pre>
+</template>
