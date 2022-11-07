757d55867ac7ebb7b65421a0d248f135e9853d7a
Eduardo San Martin Morote <posva13@gmail.com>
2022-11-07 11:33:30 +0100

    chore: remove old test files

--- old_tests/core/shared.spec.ts
+++
@@ -1,7 +0,0 @@
-import { walkSet } from '../../src/core'
-
-describe('shared tools', () => {
-  it('simple walkset nested access', () => {
-    expect(walkSet({ a: { b: true } }, 'a.b', 2)).toBe(2)
-  })
-})
