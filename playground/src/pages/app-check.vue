376d710ef58fe99dc9e8b1dd9f7e88624e41c42e
Eduardo San Martin Morote <posva13@gmail.com>
2022-11-10 23:16:52 +0100

    chore: example with app check

---
+++ playground/src/pages/app-check.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import { useAppCheckToken } from 'vuefire'
+
+const token = useAppCheckToken()
+</script>
+
+<template>
+  <div>
+    <h1>App Check</h1>
+    <p>Token: {{ token }}</p>
+  </div>
+</template>
