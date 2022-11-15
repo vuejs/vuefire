6a92995e5742dacfa227c3bce59c3cde0f18515d
Eduardo San Martin Morote <posva13@gmail.com>
2022-11-15 10:00:07 +0100

    build(nuxt): fix

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath } from 'node:url'
 import { resolve } from 'path'
 import { addPlugin, defineNuxtModule } from '@nuxt/kit'
+import { type NuxtModule } from '@nuxt/schema'
 import { type FirebaseOptions } from '@firebase/app-types'
 
 export interface VueFireNuxtModuleOptions {
@@ -20,27 +21,30 @@ export interface VueFireNuxtModuleOptions {
   }
 }
 
-export default defineNuxtModule<VueFireNuxtModuleOptions>({
-  meta: {
-    name: 'vuefire',
-    configKey: 'vuefire',
-    compatibility: {
-      nuxt: '^3.0.0-0',
+const VueFireModule: NuxtModule<VueFireNuxtModuleOptions> =
+  defineNuxtModule<VueFireNuxtModuleOptions>({
+    meta: {
+      name: 'vuefire',
+      configKey: 'vuefire',
+      compatibility: {
+        nuxt: '^3.0.0-0',
+      },
     },
-  },
 
-  defaults: {
-    optionsApiPlugin: false,
-    config: {},
-    services: {},
-  },
+    defaults: {
+      optionsApiPlugin: false,
+      config: {},
+      services: {},
+    },
 
-  setup(options, nuxt) {
-    const runtimeDir = fileURLToPath(new URL('./runtime', import.meta.url))
-    if (options.optionsApiPlugin) {
-      nuxt.options.build.transpile.push(runtimeDir)
-      // TODO: check for individual options
-      addPlugin(resolve(runtimeDir, 'plugin'))
-    }
-  },
-})
+    setup(options, nuxt) {
+      const runtimeDir = fileURLToPath(new URL('./runtime', import.meta.url))
+      if (options.optionsApiPlugin) {
+        nuxt.options.build.transpile.push(runtimeDir)
+        // TODO: check for individual options
+        addPlugin(resolve(runtimeDir, 'plugin'))
+      }
+    },
+  })
+
+export default VueFireModule
