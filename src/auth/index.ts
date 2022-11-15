256907e15cb6381ba61ffd9bb12f5e9c2699ea05
Eduardo San Martin Morote <posva13@gmail.com>
2022-11-15 15:58:38 +0100

    docs: api VueFireAuth

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -7,7 +7,25 @@ import { AuthUserInjectSymbol, setupOnAuthStateChanged } from './user'
 
 export { setupOnAuthStateChanged, useCurrentUser } from './user'
 
+/**
+ * VueFire Auth Module to be added to the `VueFire` Vue plugin options.
+ *
+ * @example
+ *
+ * ```ts
+ * import { createApp } from 'vue'
+ * import { VueFire, VueFireAuth } from 'vuefire'
+ *
+ * const app = createApp(App)
+ * app.use(VueFire, {
+ *   modules: [VueFireAuth()],
+ * })
+ * ```
+ */
 export function VueFireAuth(_app?: never) {
+  //                        ^
+  // app: never to prevent the user from just passing `VueFireAuth` without calling the function
+
   // TODO: refactor to share across modules
   if (process.env.NODE_ENV !== 'production') {
     if (_app != null) {
@@ -18,7 +36,7 @@ modules: [VueFireAuth()]`)
 
   return (firebaseApp: FirebaseApp, app: App) => {
     const user = getGlobalScope(firebaseApp, app).run(() =>
-      shallowRef<User | null | undefined>()
+      shallowRef<User | null | undefined>(),
     )!
     // userMap.set(app, user)
     app.provide(AuthUserInjectSymbol, user)
