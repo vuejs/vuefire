7c214418c65a0db5eee83020fefa44d8cb5e2ba1
Eduardo San Martin Morote <posva13@gmail.com>
2022-11-29 16:05:00 +0100

    chore: loading state demo

--- playground/src/pages/config.vue
+++ playground/src/pages/config.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { doc, getDoc } from 'firebase/firestore'
 import { useDocument, useFirestore, usePendingPromises } from 'vuefire'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 
 const db = useFirestore()
 const configRef = doc(db, 'configs', 'jORwjIykFo2NmkdzTkhU')
@@ -9,21 +9,19 @@ const configRef = doc(db, 'configs', 'jORwjIykFo2NmkdzTkhU')
 const isDoneFetching = ref(false)
 const isAllDoneFetching = ref(false)
 
-getDoc(configRef).then((data) => {
-  console.log('got data once', data)
-})
+const { data: config, promise } = useDocument(configRef)
 
-const { data: config, promise } = useDocument(configRef, { wait: true })
-// const { data: hey } = useDocument(configRef)
+onMounted(() => {
+  console.log('heeyo')
+  promise.value.then((data) => {
+    console.log('one', data)
+    isDoneFetching.value = true
+  })
 
-promise.value.then((data) => {
-  console.log('one', data)
-  isDoneFetching.value = true
-})
-
-usePendingPromises().then((data) => {
-  console.log(data)
-  isAllDoneFetching.value = true
+  usePendingPromises().then((data) => {
+    console.log(data)
+    isAllDoneFetching.value = true
+  })
 })
 </script>
 
