500b37ef03df55369c80ad5b9e310765606debb3
Eduardo San Martin Morote <posva13@gmail.com>
2022-12-16 15:45:59 +0100

    chore: logs

--- packages/nuxt/src/runtime/auth/plugin.server.ts
+++ packages/nuxt/src/runtime/auth/plugin.server.ts
@@ -3,6 +3,7 @@ import type { User } from 'firebase/auth'
 import { VueFireAuthServer } from 'vuefire/server'
 import { UserSymbol } from '../admin/plugin-auth-user.server'
 import { defineNuxtPlugin } from '#app'
+import { log } from '../../logging'
 
 /**
  * Setups the auth state based on the cookie.
@@ -11,6 +12,8 @@ export default defineNuxtPlugin((nuxtApp) => {
   const firebaseApp = nuxtApp.$firebaseApp as FirebaseApp
   // @ts-expect-error: this is a private symbol
   const user = nuxtApp[UserSymbol] as User | undefined | null
+
+  log('setting up user for app', firebaseApp.name, user?.uid)
 
   // provide the user data to the app during ssr
   VueFireAuthServer(firebaseApp, nuxtApp.vueApp, user)
