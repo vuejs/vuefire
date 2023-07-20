42ba29df20af89cd79dfa0b63f3ef3a4ef2c0856
Eduardo San Martin Morote <posva13@gmail.com>
2023-07-20 09:04:14 +0200

    refactor: move lru-cache

---
+++ packages/nuxt/src/runtime/app/lru-cache.ts
@@ -0,0 +1,18 @@
+import { deleteApp, type FirebaseApp } from 'firebase/app'
+import { LRUCache } from 'lru-cache'
+
+// TODO: allow customizing
+// TODO: find sensible defaults. Should they change depending on the platform?
+
+// copied from https://github.com/FirebaseExtended/firebase-framework-tools/blob/e69f5bdd44695274ad88dbb4e21aac778ba60cc8/src/constants.ts
+export const LRU_MAX_INSTANCES = 100
+export const LRU_TTL = 1_000 * 60 * 5
+export const appCache = new LRUCache<string, FirebaseApp>({
+  max: LRU_MAX_INSTANCES,
+  ttl: LRU_TTL,
+  allowStale: true,
+  updateAgeOnGet: true,
+  dispose: (value) => {
+    deleteApp(value)
+  },
+})
