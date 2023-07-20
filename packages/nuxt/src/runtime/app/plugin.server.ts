590bdb20feb75e72522158d4f246973112206dcd
Eduardo San Martin Morote <posva13@gmail.com>
2023-07-20 09:04:14 +0200

    refactor: move lru-cache

--- packages/nuxt/src/runtime/app/plugin.server.ts
+++ packages/nuxt/src/runtime/app/plugin.server.ts
@@ -1,25 +1,10 @@
-import { deleteApp, type FirebaseApp, initializeApp } from 'firebase/app'
+import { type FirebaseApp, initializeApp } from 'firebase/app'
 import { type User } from 'firebase/auth'
 import { DecodedIdToken } from 'firebase-admin/auth'
-import { LRUCache } from 'lru-cache'
 import { logger } from '../logging'
 import { DECODED_ID_TOKEN_SYMBOL } from '../constants'
+import { appCache } from './lru-cache'
 import { defineNuxtPlugin, useAppConfig } from '#app'
-
-// TODO: allow customizing
-// TODO: find sensible defaults. Should they change depending on the platform?
-// copied from https://github.com/FirebaseExtended/firebase-framework-tools/blob/e69f5bdd44695274ad88dbb4e21aac778ba60cc8/src/constants.ts
-export const LRU_MAX_INSTANCES = 100
-export const LRU_TTL = 1_000 * 60 * 5
-const appCache = new LRUCache<string, FirebaseApp>({
-  max: LRU_MAX_INSTANCES,
-  ttl: LRU_TTL,
-  allowStale: true,
-  updateAgeOnGet: true,
-  dispose: (value) => {
-    deleteApp(value)
-  },
-})
 
 /**
  * Initializes the app and provides it to others.
