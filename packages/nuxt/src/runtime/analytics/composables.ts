08243680817685ed606c68156dd92f2a05485d0d
Eduardo San Martin Morote <posva13@gmail.com>
2023-07-21 17:00:21 +0200

    chore: add wip analytics to nuxt

---
+++ packages/nuxt/src/runtime/analytics/composables.ts
@@ -0,0 +1,12 @@
+import { getAnalytics } from 'firebase/analytics'
+import { useFirebaseApp } from '../app/composables'
+
+/**
+ * Retrieves the Firebase analytics instance. **Returns `null` on the server** and when the platform isn't supported.
+ *
+ * @param name - name of the application
+ * @returns the Analytics instance
+ */
+export function useAnalytics() {
+  return process.client ? getAnalytics(useFirebaseApp()) : null
+}
