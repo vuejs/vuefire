31c95109b2a4423258ceb8f273dd6a57c14eccd4
Eduardo San Martin Morote <posva13@gmail.com>
2023-08-29 09:26:03 +0200

    build: use d.mts

--- packages/nuxt/build.config.ts
+++ packages/nuxt/build.config.ts
@@ -1,6 +1,7 @@
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
+  clean: true,
   // explicitly externalize consola since Nuxt has it
   externals: ['consola'],
 })
