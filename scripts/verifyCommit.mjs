e0410e567246cf11795a34118cdde04bbb386e7b
Eduardo San Martin Morote <posva13@gmail.com>
2023-12-30 08:46:28 +0100

    chore: simple-git-hooks

--- scripts/verifyCommit.mjs
+++ scripts/verifyCommit.mjs
@@ -1,34 +1,27 @@
-// Invoked on the commit-msg git hook by yorkie.
+// @ts-check
+import { readFileSync } from 'node:fs'
+import path from 'node:path'
 
-import chalk from 'chalk'
-const msgPath = process.env.GIT_PARAMS
-import { readFile } from 'fs/promises'
+// Define raw escape codes for colors
+const reset = '\x1b[0m'
+const red = '\x1b[31m'
+const green = '\x1b[32m'
+const bgRedWhite = '\x1b[41m\x1b[37m'
 
-async function main() {
-  const msg = (await readFile(msgPath, 'utf-8')).trim()
+const msgPath = path.resolve('.git/COMMIT_EDITMSG')
+const msg = readFileSync(msgPath, 'utf-8').trim()
 
-  const commitRE =
-    /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/
+const commitRE =
+  /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/
 
-  if (!commitRE.test(msg)) {
-    console.log()
-    console.error(
-      `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(
-        `invalid commit message format.`
-      )}\n\n` +
-        chalk.red(
-          `  Proper commit message format is required for automated changelog generation. Examples:\n\n`
-        ) +
-        `    ${chalk.green(
-          `fix(view): handle keep-alive with aborted navigations`
-        )}\n` +
-        `    ${chalk.green(
-          `fix(view): handle keep-alive with aborted navigations (close #28)`
-        )}\n\n` +
-        chalk.red(`  See .github/commit-convention.md for more details.\n`)
-    )
-    process.exit(1)
-  }
+if (!commitRE.test(msg)) {
+  console.log()
+  console.error(
+    `  ${bgRedWhite} ERROR ${reset} ${red}invalid commit message format.${reset}\n\n` +
+      `${red}  Proper commit message format is required for automated changelog generation. Examples:\n\n` +
+      `    ${green}feat: add disableRoot option${reset}\n` +
+      `    ${green}fix(view): handle keep-alive with aborted navigations (close #28)${reset}\n\n` +
+      `${red}  See .github/commit-convention.md for more details.${reset}\n`
+  )
+  process.exit(1)
 }
-
-main()
