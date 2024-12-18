3657ba48c240bec574bf565b255138015845d8ac
Eduardo San Martin Morote <posva13@gmail.com>
2024-12-18 17:11:51 +0100

    chore: update eslint

---
+++ packages/nuxt/eslint.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
+
+// Run `npx @eslint/config-inspector` to inspect the resolved config interactively
+export default createConfigForNuxt({
+  features: {
+    // Rules for module authors
+    tooling: true,
+    // Rules for formatting
+    stylistic: true,
+  },
+  dirs: {
+    src: ['./playground'],
+  },
+}).append({
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+})
